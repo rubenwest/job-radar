f69b97d6a5a1419715a0ce860bb1c9e7984ae672
rubenwest <rubensg90@gmail.com>
2025-07-30 22:51:35 +0200

    feat: config next and create job interface

---
+++ src/types/job.ts
@@ -0,0 +1,11 @@
+export interface Job {
+  id: number;
+  date: string;
+  company: string;
+  position: string;
+  location: string;
+  tags: string[];
+  logo: string;
+  url: string;
+  description: string;
+}
